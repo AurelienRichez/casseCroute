bcfe3d906775b8c4fd80ecbdb0537405e61941f7
aurelien <aurelien@172.16.127.197>
2012-03-29 23:39:17 +0200

    création de la base de test complète

--- test/creationBaseTest.php
+++ test/creationBaseTest.php
@@ -24,10 +24,42 @@ function creer() {
 (10, "cafe", 7, 2);');
 	
 	
-	$req = file_get_contents('../install/install_db.sql');
-	$db->query($req)or die(print_r($db->errorInfo()));
+	$db->query("CREATE TABLE calendar (
+	day DATE,
+	PRIMARY KEY(day)
+)");
+
+$db->query("CREATE TABLE orders (
+	id_order INT AUTO_INCREMENT,
+	id_user CHAR(30),
+	name_user CHAR(30),
+	surname_user CHAR(30),
+	day date,
+	PRIMARY KEY(id_order),
+	FOREIGN KEY(day)
+		REFERENCES calendar(day)
+		ON DELETE RESTRICT
+)");
+
+$db->query("CREATE TABLE ordered_products(
+	id_order INT,
+	id_product decimal(10,0),
+	nb_product INT,
+	PRIMARY KEY(id_order,id_product),
+	FOREIGN KEY(id_order)
+		REFERENCES orders(id_order)
+		ON DELETE CASCADE,
+	FOREIGN KEY(id_product)
+		REFERENCES articles(code)
+		ON DELETE CASCADE
+)")or die(print_r($db->errorInfo()));
 	
 	//TODO remplir la base
+	
+	$db->exec("INSERT INTO calendar VALUES('2012-05-03')") or die(print_r($db->errorInfo()));
+	$db->exec('INSERT INTO orders(id_user,surname_user,name_user,day) VALUES("ariche10","Richez","Aurélien","2012-05-03")') or die(print_r($db->errorInfo()));
+	$id = $db->lastInsertId();
+	$db->exec('INSERT INTO ordered_products(id_order,id_product,nb_product) VALUES('.$id.',10,2)') or die(print_r($db->errorInfo()));
 	
 	
 	return $db;
