dac14484050b3e35c18d263460d446644d556ff1
aurelien <aurelien.richez@gmail.com>
2012-04-01 01:07:28 +0200

    création des controleurs

---
+++ controller/accueil.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * controller accueil.php 
+ *
+ */
+
+/*
+ * la session ainsi que l'objet user sont censé avoir été créé au préalable
+ * par le controleur du site. Ce controleur doit être appelé depuis index.php
+ * à la racine du site.
+ */
+
+/*
+ * Ce controleur se contente d'afficher la page d'accueil en appelant la vue, aucun traitement spécial à effectuer sur cette page.
+ */
+ 
